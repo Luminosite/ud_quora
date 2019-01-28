434bfff617e08c6e7a91c34a730fd45d8d211cdb
kunfu <kunfu@paypal.com>
2019-01-28 17:23:41 +0800

    finish multi process

--- multi_process.py
+++ multi_process.py
@@ -1,6 +1,8 @@
-import Queue
+import queue
 import threading
 import time
+import pandas as pd
+import numpy as np
 
 
 class WorkThread (threading.Thread):
@@ -15,19 +17,19 @@ class WorkThread (threading.Thread):
         
     def run(self):
         print("thread %d for %s starts" % (self.index, self.tag))
-        ret = self.func(data)
+        ret = self.func(self.data)
         self.lock.acquire()
         self.q.put((self.index, ret))
         self.lock.release()
         print("thread %d for %s finished" % (self.index, self.tag))
         
         
-def process_data(func, data, tag, n):
+def process_data(func, data, n, tag):
     queueLock = threading.Lock()
-    workQueue = Queue.Queue(n)
+    workQueue = queue.Queue(n)
     
     l = data.shape[0]
-    block_len = (l-1)/n + 1
+    block_len = int((l-1)/n) + 1
     
     for i in range(n):
         d = data[i*block_len: (i+1)*block_len]
@@ -44,8 +46,20 @@ def process_data(func, data, tag, n):
             ret = [x[1] for x in ret]
             result = pd.concat(ret, axis=0).reset_index(drop=True)
             finish = True
-        else:
-            time.sleep(3)
         queueLock.release()
+        if not finish:
+            time.sleep(1)
     
     return result
+
+
+def testFunc(data):
+    time.sleep(3)
+    return data*(-1)
+
+
+if __name__ == '__main__':
+    for _ in range(100):
+        d = pd.DataFrame(np.array(range(29)))
+        d = process_data(testFunc, d, 5, "test func")
+        print(d)
