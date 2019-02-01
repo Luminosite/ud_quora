d7c75d3562f17cbf449655f06326f4e89d9f3fdc
kunfu <kunfu@paypal.com>
2019-02-01 22:13:13 +0800

    refactor feature multi-process

--- features_mp.py
+++ features_mp.py
@@ -1,5 +1,5 @@
 from math import log
-from poolprocess import process_data # func data n t
+from poolprocess import process_data
 import _pickle as cPickle
 import pandas as pd
 import numpy as np
@@ -11,6 +11,7 @@ from tqdm import tqdm
 from scipy.stats import skew, kurtosis
 from scipy.spatial.distance import cosine, cityblock, jaccard, canberra, euclidean, minkowski, braycurtis
 from nltk import word_tokenize
+import os
 
 # from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.decomposition import TruncatedSVD
@@ -18,11 +19,12 @@ from sklearn.decomposition import NMF
 # from sklearn.decomposition import PCA
 # from sklearn.feature_extraction import text
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-stop_words = stopwords.words('english')
 from time import time
 
-block_size = 400000 # evaluation for size of train.csv
+stop_words = stopwords.words('english')
+
+# evaluation for size of train.csv
+block_size = 400000
 
 
 def time_cnt(f, tag="func"):
@@ -35,21 +37,19 @@ def time_cnt(f, tag="func"):
     return ret
 
 
-def getLogger():
+def get_logger():
     import logging
-    LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+    log_format = "%(asctime)s - %(levelname)s - %(message)s"
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(LOG_FORMAT)
+    formatter = logging.Formatter(log_format)
     ch.setFormatter(formatter)
-    logger.handlers=[]
+    logger.handlers = []
     logger.addHandler(ch)
     return logger
 
-
-import os
 
 def read_or_gen_by_list(l):
     current = len(l) - 1
@@ -69,7 +69,6 @@ def r_or_g_by_list_n(l, i):
         print("data for {name} read".format(name=name))
         return d
     else:
-        d = None
         print("generate the data for {name}".format(name=name))
         if i == 0:
             d = func()
@@ -84,7 +83,6 @@ def r_or_g_by_list_n(l, i):
 def wmd(s1, s2, model):
     s1 = str(s1).lower().split()
     s2 = str(s2).lower().split()
-    stop_words = stopwords.words('english')
     s1 = [w for w in s1 if w not in stop_words]
     s2 = [w for w in s2 if w not in stop_words]
     return model.wmdistance(s1, s2)
@@ -93,7 +91,6 @@ def wmd(s1, s2, model):
 def norm_wmd(s1, s2, norm_model):
     s1 = str(s1).lower().split()
     s2 = str(s2).lower().split()
-    stop_words = stopwords.words('english')
     s1 = [w for w in s1 if w not in stop_words]
     s2 = [w for w in s2 if w not in stop_words]
     return norm_model.wmdistance(s1, s2)
@@ -112,6 +109,7 @@ def lemmatize_all(sentence):
             yield wnl.lemmatize(word, pos='r')
         else:
             yield word
+
 
 def rebuild_sentence(s):
     sentence = str(s).lower()
@@ -135,14 +133,20 @@ def base_process_thread(data):
     data['len_char_q2'] = data.question2.apply(lambda x: len(''.join(set(str(x).replace(' ', '')))))
     data['len_word_q1'] = data.question1.apply(lambda x: len(str(x).split()))
     data['len_word_q2'] = data.question2.apply(lambda x: len(str(x).split()))
-    data['common_words'] = data.apply(lambda x: len(set(str(x['question1']).lower().split()).intersection(set(str(x['question2']).lower().split()))), axis=1)
+    data['common_words'] = data.apply(lambda x: len(set(str(x['question1']).lower().split())
+                                                    .intersection(set(str(x['question2']).lower().split()))), axis=1)
     data['fuzz_qratio'] = data.apply(lambda x: fuzz.QRatio(str(x['question1']), str(x['question2'])), axis=1)
     data['fuzz_WRatio'] = data.apply(lambda x: fuzz.WRatio(str(x['question1']), str(x['question2'])), axis=1)
-    data['fuzz_partial_ratio'] = data.apply(lambda x: fuzz.partial_ratio(str(x['question1']), str(x['question2'])), axis=1)
-    data['fuzz_partial_token_set_ratio'] = data.apply(lambda x: fuzz.partial_token_set_ratio(str(x['question1']), str(x['question2'])), axis=1)
-    data['fuzz_partial_token_sort_ratio'] = data.apply(lambda x: fuzz.partial_token_sort_ratio(str(x['question1']), str(x['question2'])), axis=1)
-    data['fuzz_token_set_ratio'] = data.apply(lambda x: fuzz.token_set_ratio(str(x['question1']), str(x['question2'])), axis=1)
-    data['fuzz_token_sort_ratio'] = data.apply(lambda x: fuzz.token_sort_ratio(str(x['question1']), str(x['question2'])), axis=1)
+    data['fuzz_partial_ratio'] = data.apply(lambda x: fuzz.partial_ratio(
+        str(x['question1']), str(x['question2'])), axis=1)
+    data['fuzz_partial_token_set_ratio'] = data.apply(lambda x: fuzz.partial_token_set_ratio(
+        str(x['question1']), str(x['question2'])), axis=1)
+    data['fuzz_partial_token_sort_ratio'] = data.apply(lambda x: fuzz.partial_token_sort_ratio(
+        str(x['question1']), str(x['question2'])), axis=1)
+    data['fuzz_token_set_ratio'] = data.apply(lambda x: fuzz.token_set_ratio(
+        str(x['question1']), str(x['question2'])), axis=1)
+    data['fuzz_token_sort_ratio'] = data.apply(lambda x: fuzz.token_sort_ratio(
+        str(x['question1']), str(x['question2'])), axis=1)
     return data
 
 
@@ -155,7 +159,7 @@ def base_feature_with_size(n, data_file, start):
         if data_file != "test.csv":
             data = data.drop(['id', 'qid1', 'qid2'], axis=1)
         print("basic data is read")
-        ret = process_data(base_process_thread, data, n=8, tag="base_feature") # func data n t
+        ret = process_data(base_process_thread, data, n=8, tag="base_feature")
         return ret
         
     return basic_feature
@@ -173,7 +177,8 @@ def wmd_feature(ret):
 
 
 def norm_wmd_tread(data):
-    norm_model = gensim.models.KeyedVectors.load_word2vec_format('data/GoogleNews-vectors-negative300.bin.gz', binary=True)
+    norm_model = gensim.models.KeyedVectors.load_word2vec_format(
+        'data/GoogleNews-vectors-negative300.bin.gz', binary=True)
     norm_model.init_sims(replace=True)
     data['norm_wmd'] = data.apply(lambda x: norm_wmd(x['question1'], x['question2'], norm_model), axis=1)
     return data
@@ -185,18 +190,18 @@ def norm_wmd_feature(ret):
 
 
 def sent2vec(s, model):
-    words = str(s).lower()#.decode('utf-8')
+    words = str(s).lower()
     words = word_tokenize(words)
-    words = [w for w in words if not w in stop_words]
+    words = [w for w in words if w not in stop_words]
     words = [w for w in words if w.isalpha()]
-    M = []
+    m = []
     for w in words:
         try:
-            M.append(model[w])
-        except:
+            m.append(model[w])
+        except Exception as _:
             continue
-    M = np.array(M)
-    v = M.sum(axis=0)
+    m = np.array(m)
+    v = m.sum(axis=0)
     return v / np.sqrt((v ** 2).sum())
 
 
@@ -207,7 +212,7 @@ def dist_thread(data):
     for i, q in tqdm(enumerate(data.question1.values)):
         question1_vectors[i, :] = sent2vec(q, model)
 
-    question2_vectors  = np.zeros((data.shape[0], 300))
+    question2_vectors = np.zeros((data.shape[0], 300))
     for i, q in tqdm(enumerate(data.question2.values)):
         question2_vectors[i, :] = sent2vec(q, model)
 
@@ -248,22 +253,6 @@ def dist_features(ret):
     return ret
 
 
-def gen_feature(n=0, data_file="train.csv", start=0):
-    tag = data_file.split('.')[0]
-    if n > 0 :
-        tag = "{t}_{start}_{end}".format(t=tag, start=start, end=n)
-    operations = [
-        ("basic_feature_{t}".format(t=tag), base_feature_with_size(n, data_file, start))
-        ,("wmd_feature_{t}".format(t=tag), wmd_feature)
-        ,("norm_wmd_feature_{t}".format(t=tag), norm_wmd_feature)
-        ,("dist_features_{t}".format(t=tag), dist_features)
-        #,("add_normal_wmd_feature", normal_wmd_feature)
-    ]
-
-    features_data = read_or_gen_by_list(operations)
-    return features_data
-
-
 def gen_ratio(col, data):
     data['{c}_ratio_ln'.format(c=col)] = data.apply(
         lambda x: log(x['{c}1'.format(c=col)]/x['{c}2'.format(c=col)]),
@@ -272,9 +261,8 @@ def gen_ratio(col, data):
 
 
 def common_ratio_thread(data):
-    data['common_ratio'] = data.apply(lambda x: 2 * float(x['common_words']) /
-                                                (float(x['len_word_q1']) + float(x['len_word_q2'])),
-                                      axis=1)
+    data['common_ratio'] = data.apply(
+        lambda x: 2*float(x['common_words']) / (float(x['len_word_q1']) + float(x['len_word_q2'])), axis=1)
     cols = ["len_q", "len_char_q", "len_word_q"]
     for col in cols:
         data = gen_ratio(col, data)
@@ -292,7 +280,7 @@ def dist_features_for(data, q1, q2, tag="tfidf"):
 
     def add_dist_for(func):
         col_name = '{d}_distance_{t}'.format(d=func.__name__, t=tag)
-        data[col_name] = [func(x, y)  for (x, y) in zip(q1, q2)]
+        data[col_name] = [func(x, y) for (x, y) in zip(q1, q2)]
 
     add_dist_for(cosine)
     add_dist_for(cityblock)
@@ -315,13 +303,13 @@ def tfidf_gen(t_data):
     train = t_data.loc[:, ft]
     
     print('Generate tfidf')
-    feats= ft
-    vect_orig = TfidfVectorizer(max_features=None,ngram_range=(1, 1), min_df=3)
+    feats = ft
+    vect_orig = TfidfVectorizer(max_features=None, ngram_range=(1, 1), min_df=3)
 
     corpus = []
     for f in feats:
         train.loc[:, f] = train.loc[:, f].astype(str)
-        corpus+=train[f].values.tolist()
+        corpus += train[f].values.tolist()
     vect_orig.fit(corpus)
     
     train_tfidf = vect_orig.transform(corpus)
@@ -330,8 +318,8 @@ def tfidf_gen(t_data):
 
 def attach_vec_directly(data, q1, q2, tag):
     width = int(q1.shape[1])
-    shortTag = tag.replace(' ', '')
-    cols = [["q{i}_{s}_{sub_num}".format(i=i, s=shortTag, sub_num=x) for x in range(width)] for i in range(1,3)]
+    short_tag = tag.replace(' ', '')
+    cols = [["q{i}_{s}_{sub_num}".format(i=i, s=short_tag, sub_num=x) for x in range(width)] for i in range(1, 3)]
     print("to attach cols:", cols)
     dq1 = pd.DataFrame(q1, columns=cols[0])
     dq2 = pd.DataFrame(q2, columns=cols[1])
@@ -344,11 +332,11 @@ def prepare_vec_dist_data(data, vec, tag="tag"):
     q2 = vec[single_set_size:]
     
     dist_features_data = data
-    if (q1.shape[1] >28 ):
+    if q1.shape[1] > 28:
         dist_features_data = dist_features_for(data, q1, q2, tag=tag)
     
     attached = dist_features_data
-    if (q1.shape[1] < 50):
+    if q1.shape[1] < 50:
         attached = attach_vec_directly(dist_features_data, q1, q2, tag)
     return attached
 
@@ -369,25 +357,34 @@ def transfer_vec_cal_dist(data, ms):
 
 def svd300(d):
     return transfer_vec_cal_dist(d, (TruncatedSVD, [300]))
+
+
 def svd300_feature(data):
-    data_block_number = int( (data.shape[0]-1) / block_size ) + 1
+    data_block_number = int((data.shape[0]-1) / block_size) + 1
     process_data(svd300, data, n=2, tag='topic model', data_n=data_block_number)
     return data
 
+
 def svd25(d):
     return transfer_vec_cal_dist(d, (TruncatedSVD, [25]))
+
+
 def svd25_feature(data):
-    data_block_number = int( (data.shape[0]-1) / block_size ) + 1
+    data_block_number = int((data.shape[0]-1) / block_size) + 1
     process_data(svd25, data, n=2, tag='topic model', data_n=data_block_number)
     return data
 
 
 def nmf30(d):
     return transfer_vec_cal_dist(d, (NMF, [30]))
+
+
 def nmf30_feature(data):
-    data_block_number = int( (data.shape[0]-1) / block_size ) + 1
+    data_block_number = int((data.shape[0]-1) / block_size) + 1
     process_data(nmf30, data, n=2, tag='topic model', data_n=data_block_number)
     return data
+
+
 ###################################################################
 # local word2vec distance
 ###################################################################
@@ -410,7 +407,7 @@ def sentence2vec(words, model, vec_size):
 
 
 def local_vec(data):
-    vec_size=200
+    vec_size = 200
     w1, w2 = get_words(data)
     words = np.concatenate((w1, w2), axis=0)
     model = gensim.models.word2vec.Word2Vec(words, min_count=5, size=vec_size, window=5)
@@ -421,14 +418,16 @@ def local_vec(data):
 
 
 def local_vec_feature(data):
-    data_block_number = int( (data.shape[0]-1) / block_size ) + 1
+    data_block_number = int((data.shape[0]-1) / block_size) + 1
     process_data(local_vec, data, n=4, tag='local word2vec', data_n=data_block_number)
     return data
+
+
 ###################################################################
 # pos vec distance
 ###################################################################
 pos_base = {'WP': 0, 'VB': 0, 'DT': 0, 'NN': 0, 'IN': 0, 'RB': 0, 'TO': 0,
-            'JJ': 0, 'WRB':0, 'MD': 0, 'PRP':0, 'CC': 0, 'VBP':0}
+            'JJ': 0, 'WRB': 0, 'MD': 0, 'PRP': 0, 'CC': 0, 'VBP': 0}
 pos_features = list(pos_base)
 
 
@@ -449,7 +448,7 @@ def pos_vec(data):
 
 
 def pos_vec_feature(data):
-    data_block_number = int( (data.shape[0]-1) / block_size ) + 1
+    data_block_number = int((data.shape[0]-1) / block_size) + 1
     process_data(pos_vec, data, n=8, tag='pos vec', data_n=data_block_number)
     return data
 ###################################################################
@@ -460,23 +459,23 @@ def pos_vec_feature(data):
 ###################################################################
 # process interface
 ###################################################################
-operations = [
-    ("basic_feature_", base_feature_with_size)
-    , ("wmd_feature_", wmd_feature)
-    , ("norm_wmd_feature_", norm_wmd_feature)
-    , ("dist_features_", dist_features)
-    , ("common_ratio_features_", common_ratio_feature)
-    , ("svd300_features_", svd300_feature)
-    , ("svd25_features_", svd25_feature)
-    , ("nmf30_features_", nmf30_feature)
-    , ("local_vec_", local_vec_feature)
-    , ("pos_vec_", pos_vec_feature)
-]
+operations = (
+    ("basic_feature_", base_feature_with_size),
+    ("wmd_feature_", wmd_feature),
+    ("norm_wmd_feature_", norm_wmd_feature),
+    ("dist_features_", dist_features),
+    ("common_ratio_features_", common_ratio_feature),
+    ("svd300_features_", svd300_feature),
+    ("svd25_features_", svd25_feature),
+    ("nmf30_features_", nmf30_feature),
+    ("local_vec_", local_vec_feature),
+    ("pos_vec_", pos_vec_feature)
+)
 
 
 def feature_generation(n=0, data_file="train.csv", start=0, operation_list=operations):
     tag_base = data_file.split('.')[0]
-    tag = tag_base if n <=0 else "{t}_{start}_{end}".format(t=tag_base, start=start, end=n)
+    tag = tag_base if n <= 0 else "{t}_{start}_{end}".format(t=tag_base, start=start, end=n)
     ops = []
     for i, itr in enumerate(operation_list):
         base_name, func = itr
